13d1c674a91fe3204ce28efac213bef333d8edda
Ace <51465755+AceVikings@users.noreply.github.com>
2022-09-14 17:48:10 +0530

    Update Token.sol

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -31,7 +31,7 @@ contract Token is ERC20{
             amount += tokens[i];
             partner[users[i]] = partnerInfo(tokens[i],false);
         }
-        require(amount <= 1_000_000 ether,"Team share can't exceed 1M");
+        require(amount <= 2_500_000 ether,"Team share can't exceed 1M");
         factory = Factory(_factory);
     }
 
